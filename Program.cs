dc377ac96fd21a1497da5d078f0b8b61f10d0e0b
Larissa <larissarf1910@gmail.com>
2024-08-12 13:48:10 -0300

    feat: :sparkles: Adiciona novas rotas no projeto

--- Program.cs
+++ Program.cs
@@ -4,8 +4,6 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Adicionar serviços ao contêiner.
-
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
 
@@ -14,6 +12,11 @@ var app = builder.Build();
 // Criar rota para listagem de livros
 app.MapGet("/api/books", async (AppDbContext db) => {
     var books = await db.Books.ToListAsync();
+    
+    if (books.Count == 0)
+    {
+        return Results.NoContent();
+    }
     return Results.Ok(books);
 });
 
@@ -21,23 +24,69 @@ app.MapGet("/api/books", async (AppDbContext db) => {
 app.MapPost("/api/books", async (AppDbContext db, Book book) => {
     db.Books.Add(book);
     await db.SaveChangesAsync();
+    // Created = 201
     return Results.Created("Created", book);
 });
 
 //Criar rota para retornar um livro específico
-app.MapGet("/api/books/{id}", (int id) => {
-    return Results.Ok($"Você está na rota de retorno do livro de id {id}");
+app.MapGet("api/books/{id}", async (AppDbContext db, int id) => {
+    var book = await db.Books.FirstOrDefaultAsync(b => b.id == id);
 
+    if (book == null)
+    {
+        return Results.NotFound("O livro não foi encontrado");
+    }
+    // Ok = 200
+    return Results.Ok(book);
+});
+
+app.MapGet("api/books/title/{title}", async (AppDbContext db, string Title) => {
+    var book = await db.Books.FirstOrDefaultAsync(b => b.Title == Title);
+
+    if (book == null)
+    {
+        return Results.NotFound($"O livro '{Title}' não foi encontrado");
+    }
+
+    return Results.Ok(book);
+});
+
+app.MapGet("api/books/author/{author}", async (AppDbContext db, string Author) => {
+    var booksByAuthor = await db.Books.Where(b => b.Author == Author).ToListAsync();
+
+    if (booksByAuthor.Count == 0)
+    {
+        return Results.NotFound($"Nenhum livro do autor '{Author}' foi encontrado");
+    }
+
+    return Results.Ok(booksByAuthor);
 });
 
 //Criar rota para atualizar um livro específico
-app.MapPut("/api/books/{id}", (int id) => {
-    return Results.Ok($"Você está na rota de atualização do livro de id {id}");
+app.MapPut("/api/books/{id}", async (AppDbContext db, int id, Book book) => {
+    var bookToUpdate = await db.Books.FindAsync(id);
+    if (bookToUpdate == null)
+    {
+        return Results.NotFound("O livro não foi encontrado");
+    }
+    bookToUpdate.Title = book.Title;
+    bookToUpdate.Author = book.Author;
+    await db.SaveChangesAsync();
+    return Results.Ok(bookToUpdate);
 });
 
 //Criar rota para deletar um livro específico
-app.MapDelete("/api/books/{id}", (int id) => {
-    return Results.Ok($"Você está na rota de deleção do livro de id {id}");
+app.MapDelete("/api/books/{id}", async (AppDbContext db, int id) => {
+   var bookToDelete = await db.Books.FindAsync(id);
+
+   if (bookToDelete == null)
+   {
+       return Results.NotFound("O livro não foi encontrado");
+   }
+
+   db.Books.Remove(bookToDelete);
+   await db.SaveChangesAsync();
+   return Results.Ok($"O livro '{bookToDelete.Title}' foi deletado");
 });
 
 app.Run();
--- obj/Debug/net8.0/BooksApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/BooksApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BooksApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df99fdc4f4dcb4d5b5e57b089adcb82cb0ddc201")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a54c861b57ab65082e8d1ebecc3324dbd5b3ea5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BooksApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BooksApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
