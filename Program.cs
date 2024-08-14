a7b7d2de8836aadaaa4a2b8c2e9cdf4fb4b4cd37
Larissa <larissarf1910@gmail.com>
2024-08-14 18:55:39 -0300

    docs: :memo: Adiciona swagger

--- Program.cs
+++ Program.cs
@@ -1,14 +1,35 @@
 using BooksAPI.Data;
-using BooksAPI.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "BooksAPI", Version = "v1" });
+});
+
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+        .AddJsonOptions(options =>
+        {
+            options.JsonSerializerOptions.ReferenceHandler = 
+            System.Text.Json.Serialization.ReferenceHandler.Preserve;
+        });
 
 var app = builder.Build();
+
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "BooksAPI v1");
+    c.RoutePrefix = string.Empty;
+});
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAuthorization();
 
 app.MapControllers();
 
